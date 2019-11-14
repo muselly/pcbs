a408b9c120d8eb8f73ecf4317a59e817216f0c1a
Eleonore <eleonore.scholler@ens.fr>
2019-11-14 10:58:15 +0100

    Changes to temperature conversion program in class.

--- farenheit.py
+++ farenheit.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Convert temperature from F to C or C to F. Input arguments are F/C and temperature to convert.
+"""
+
 import sys
 
-tempF = int(sys.argv[1])
 
-tempC = (tempF - 32) * 5/(9)
 
-print(tempC)
+if sys.argv[1]=="F":
+    temp = (int(sys.argv[2])-32)*5/9
+    print(f"{temp:.2f} C")
+
+if sys.argv[1]=="C":
+    temp = int(sys.argv[2]) *9/5 + 32
+    print(round(temp,2),"F")
